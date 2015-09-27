akafinch/stk-code

commit 7a88d159df94ef08228ed717da936d30c45bc3c7

Removed Shaders instance out of irr-driver, and use a class with static members instead.

src/graphics/irr_driver.cpp
@@ -115,7 +115,6 @@ IrrDriver::IrrDriver()
                                          /*event receiver*/ NULL,
                                          file_manager->getFileSystem());
     m_request_screenshot = false;
-    m_shaders             = NULL;
     m_rtts                = NULL;
     m_post_processing     = NULL;
     m_wind                = new Wind();
@@ -153,7 +152,7 @@ IrrDriver::~IrrDriver()
     m_device = NULL;
     m_modes.clear();
 
-    delete m_shaders;
+    Shaders::destroy();
     delete m_wind;
 }   // ~IrrDriver
 
@@ -515,7 +514,7 @@ void IrrDriver::initDevice()
     // m_glsl might be reset in rtt if an error occurs.
     if (CVS->isGLSL())
     {
-        m_shaders = new Shaders();
+        Shaders::init();
 
         m_mrt.clear();
         m_mrt.reallocate(2);
@@ -628,7 +627,7 @@ void IrrDriver::createSunInterposer()
     m_sun_interposer->getMaterial(0).Lighting = false;
     m_sun_interposer->getMaterial(0).ColorMask = video::ECP_NONE;
     m_sun_interposer->getMaterial(0).ZWriteEnable = false;
-    m_sun_interposer->getMaterial(0).MaterialType = m_shaders->getShader(ES_OBJECTPASS);
+    m_sun_interposer->getMaterial(0).MaterialType = Shaders::getShader(ES_OBJECTPASS);
 
     sphere->drop();
 }
@@ -781,7 +780,7 @@ void IrrDriver::applyResolutionSettings()
     // FIXME: this load sequence is (mostly) duplicated from main.cpp!!
     // That's just error prone
     // (we're sure to update main.cpp at some point and forget this one...)
-    m_shaders->killShaders();
+    Shaders::destroy();
     VAOManager::getInstance()->kill();
     SolidPassCmd::getInstance()->kill();
     ShadowPassCmd::getInstance()->kill();
@@ -2425,13 +2424,13 @@ void IrrDriver::applyObjectPassShader(scene::ISceneNode * const node, bool rimli
 
     const u32 mcount = node->getMaterialCount();
     u32 i;
-    const video::E_MATERIAL_TYPE ref = rimlit ? m_shaders->getShader(ES_OBJECTPASS_RIMLIT):
-                                       m_shaders->getShader(ES_OBJECTPASS_REF);
-    const video::E_MATERIAL_TYPE pass = rimlit ? m_shaders->getShader(ES_OBJECTPASS_RIMLIT):
-                                        m_shaders->getShader(ES_OBJECTPASS);
+    const video::E_MATERIAL_TYPE ref = Shaders::getShader(rimlit ? ES_OBJECTPASS_RIMLIT
+                                                                 : ES_OBJECTPASS_REF);
+    const video::E_MATERIAL_TYPE pass = Shaders::getShader(rimlit ? ES_OBJECTPASS_RIMLIT 
+                                                                  : ES_OBJECTPASS);
 
-    const video::E_MATERIAL_TYPE origref = m_shaders->getShader(ES_OBJECTPASS_REF);
-    const video::E_MATERIAL_TYPE origpass = m_shaders->getShader(ES_OBJECTPASS);
+    const video::E_MATERIAL_TYPE origref = Shaders::getShader(ES_OBJECTPASS_REF);
+    const video::E_MATERIAL_TYPE origpass = Shaders::getShader(ES_OBJECTPASS);
 
     bool viamb = false;
     scene::IMesh *mesh = NULL;
@@ -2521,7 +2520,7 @@ scene::ISceneNode *IrrDriver::addLight(const core::vector3df &pos, float energy,
 
             m_rsm_matrix_initialized = false;
 
-            ((WaterShaderProvider *) m_shaders->m_callbacks[ES_WATER])->setSunPosition(pos);
+            ((WaterShaderProvider *) Shaders::getCallback(ES_WATER) )->setSunPosition(pos);
         }
 
         return light;

src/graphics/2dutils.cpp
@@ -16,7 +16,9 @@
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "2dutils.hpp"
-#include "central_settings.hpp"
+
+#include "graphics/central_settings.hpp"
+#include "graphics/shaders.hpp"
 #include "glwrap.hpp"
 #include "utils/cpp2011.hpp"
 

src/graphics/central_settings.cpp
@@ -15,11 +15,13 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#include "central_settings.hpp"
+#include "graphics/central_settings.hpp"
+
+#include "config/user_config.hpp"
 #include "modes/profile_world.hpp"
-#include "gl_headers.hpp"
-#include "glwrap.hpp"
-#include "graphics_restrictions.hpp"
+#include "graphics/gl_headers.hpp"
+#include "graphics/glwrap.hpp"
+#include "graphics/graphics_restrictions.hpp"
 
 CentralVideoSettings *CVS = new CentralVideoSettings();
 

src/graphics/glwrap.cpp
@@ -15,11 +15,13 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#include "graphics/central_settings.hpp"
 #include "graphics/glwrap.hpp"
 
-#include "config/user_config.hpp"
+
 #include "config/hardware_stats.hpp"
+#include "config/user_config.hpp"
+#include "graphics/central_settings.hpp"
+#include "graphics/shaders.hpp"
 #include "graphics/stkmesh.hpp"
 #include "utils/profiler.hpp"
 #include "utils/cpp2011.hpp"

src/graphics/gpuparticles.cpp
@@ -15,15 +15,19 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#include "graphics/irr_driver.hpp"
-#include "graphics/glwrap.hpp"
-#include "gpuparticles.hpp"
-#include "io/file_manager.hpp"
+#include "graphics/gpuparticles.hpp"
+
 #include "config/user_config.hpp"
+#include "graphics/glwrap.hpp"
+#include "graphics/irr_driver.hpp"
+#include "graphics/particle_emitter.hpp"
+#include "graphics/shaders.hpp"
+#include "graphics/texture_read.hpp"
+#include "guiengine/engine.hpp"
+#include "io/file_manager.hpp"
+
 #include <ICameraSceneNode.h>
 #include <IParticleSystemSceneNode.h>
-#include "guiengine/engine.hpp"
-#include "graphics/particle_emitter.hpp"
 #include "../../lib/irrlicht/source/Irrlicht/os.h"
 #define COMPONENTCOUNT 8
 

src/graphics/gpuparticles.hpp
@@ -26,8 +26,7 @@
 
 namespace irr { namespace video{ class ITexture; } }
 
-
-
+using namespace irr;
 
 class ParticleSystemProxy : public scene::CParticleSystemSceneNode
 {

src/graphics/irr_driver.hpp
@@ -36,8 +36,8 @@
 #include <SColor.h>
 #include "IrrlichtDevice.h"
 #include "ISkinnedMesh.h"
-#include "graphics/shaders.hpp"
 #include "graphics/wind.hpp"
+#include "graphics/gl_headers.hpp"
 #include "io/file_manager.hpp"
 #include "utils/aligned_array.hpp"
 #include "utils/no_copy.hpp"
@@ -195,8 +195,7 @@ private:
     gui::IGUIFont              *m_race_font;
     /** Post-processing. */
     PostProcessing             *m_post_processing;
-    /** Shaders. */
-    Shaders              *m_shaders;
+
     /** Wind. */
     Wind                 *m_wind;
     /** RTTs. */
@@ -556,13 +555,6 @@ public:
     void setSunColor(const video::SColorf &col)
     {
         m_suncolor = col;
-    }
-    // -----------------------------------------------------------------------
-    inline video::E_MATERIAL_TYPE getShader(const ShaderType num)  {return m_shaders->getShader(num);}
-    // -----------------------------------------------------------------------
-    inline video::IShaderConstantSetCallBack* getCallback(const ShaderType num)
-    {
-        return (m_shaders == NULL ? NULL : m_shaders->m_callbacks[num]);
     }
     // ------------------------------------------------------------------------
     GLuint getRenderTargetTexture(TypeRTT which);

src/graphics/material.cpp
@@ -723,7 +723,7 @@ void  Material::setMaterialProperties(video::SMaterial *m, scene::IMeshBuffer* m
         switch (m_shader_type)
         {
         case SHADERTYPE_SOLID_UNLIT:
-            m->MaterialType = irr_driver->getShader(ES_OBJECT_UNLIT);
+            m->MaterialType = Shaders::getShader(ES_OBJECT_UNLIT);
             m->setTexture(1, glossytex);
             return;
         case SHADERTYPE_ALPHA_TEST:
@@ -747,7 +747,7 @@ void  Material::setMaterialProperties(video::SMaterial *m, scene::IMeshBuffer* m
                 video::EAS_VERTEX_COLOR);
             return;
         case SHADERTYPE_SPHERE_MAP:
-            m->MaterialType = irr_driver->getShader(ES_SPHERE_MAP);
+            m->MaterialType = Shaders::getShader(ES_SPHERE_MAP);
             m->setTexture(1, glossytex);
             return;
         case SHADERTYPE_SPLATTING:
@@ -774,7 +774,7 @@ void  Material::setMaterialProperties(video::SMaterial *m, scene::IMeshBuffer* m
             m->setTexture(6, glossytex);
 
             // Material and shaders
-            m->MaterialType = irr_driver->getShader(ES_SPLATTING);
+            m->MaterialType = Shaders::getShader(ES_SPLATTING);
             return;
         case SHADERTYPE_WATER:
             m->setTexture(1, irr_driver->getTexture(FileManager::TEXTURE,
@@ -782,18 +782,18 @@ void  Material::setMaterialProperties(video::SMaterial *m, scene::IMeshBuffer* m
             m->setTexture(2, irr_driver->getTexture(FileManager::TEXTURE,
                 "waternormals2.jpg"));
 
-            ((WaterShaderProvider *)irr_driver->getCallback(ES_WATER))->
+            ((WaterShaderProvider *)Shaders::getCallback(ES_WATER))->
                 setSpeed(m_water_shader_speed_1 / 100.0f, m_water_shader_speed_2 / 100.0f);
 
-            m->MaterialType = irr_driver->getShader(ES_WATER);
+            m->MaterialType = Shaders::getShader(ES_WATER);
             return;
         case SHADERTYPE_VEGETATION:
             // Only one grass speed & amplitude per map for now
-            ((GrassShaderProvider *)irr_driver->getCallback(ES_GRASS))->
+            ((GrassShaderProvider *)Shaders::getCallback(ES_GRASS))->
                 setSpeed(m_grass_speed);
-            ((GrassShaderProvider *)irr_driver->getCallback(ES_GRASS))->
+            ((GrassShaderProvider *)Shaders::getCallback(ES_GRASS))->
                 setAmplitude(m_grass_amplitude);
-            m->MaterialType = irr_driver->getShader(ES_GRASS_REF);
+            m->MaterialType = Shaders::getShader(ES_GRASS_REF);
             m->setTexture(1, glossytex);
             return;
         }
@@ -807,7 +807,7 @@ void  Material::setMaterialProperties(video::SMaterial *m, scene::IMeshBuffer* m
             m->setTexture(2, tex);
 
             // Material and shaders
-            m->MaterialType = irr_driver->getShader(ES_NORMAL_MAP);
+            m->MaterialType = Shaders::getShader(ES_NORMAL_MAP);
             m->setTexture(1, glossytex);
             return;
         }

commit 30150f9205df0ee658dbcd2c98b7454626ebcb99

Fixed crash on exit and changing resolution with old opengl.
Regression introduced in commit 7a88d159df94ef08228ed717da936d30c45bc3c7.
Playing the real game is still not working.

src/graphics/irr_driver.cpp
@@ -157,7 +157,10 @@ IrrDriver::~IrrDriver()
 
     delete m_shadow_matrices;
     m_shadow_matrices = NULL;
-    Shaders::destroy();
+    if (CVS->isGLSL())
+    {
+        Shaders::destroy();
+    }
     delete m_wind;
     delete m_spherical_harmonics;
 }   // ~IrrDriver
@@ -822,7 +825,10 @@ void IrrDriver::applyResolutionSettings()
     GlowPassCmd::getInstance()->kill();
     resetTextureTable();
     // initDevice will drop the current device.
-    Shaders::destroy();
+    if (CVS->isGLSL())
+    {
+        Shaders::destroy();
+    }
     initDevice();
 
     // Re-init GUI engine