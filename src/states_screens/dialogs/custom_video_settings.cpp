akafinch/stk-code

commit c039b818f357e2d8f6295b2c96e47d147e6879f6

Replaced Widget's set(De)Activated functions with one setActive(bool) function, which simplifies code for various screen a lot.

src/states_screens/dialogs/custom_video_settings.cpp
@@ -107,13 +107,13 @@ void CustomVideoSettingsDialog::beforeAddingWidgets()
     {
         CheckBoxWidget* cb_tex_cmp = getWidget<CheckBoxWidget>("texture_compression");
         cb_tex_cmp->setState(false);
-        cb_tex_cmp->setDeactivated();
+        cb_tex_cmp->setActive(false);
     }
 
     if (!CVS->supportsGlobalIllumination())
     {
-        shadows->setDeactivated();
-        getWidget<CheckBoxWidget>("global_illumination")->setDeactivated();
+        shadows->setActive(false);
+        getWidget<CheckBoxWidget>("global_illumination")->setActive(false);
     }
 }
 
@@ -227,37 +227,22 @@ GUIEngine::EventPropagation CustomVideoSettingsDialog::processEvent(const std::s
 
 void CustomVideoSettingsDialog::updateActivation()
 {
-    if(getWidget<CheckBoxWidget>("dynamiclight")->getState())
-    {
-        getWidget<CheckBoxWidget>("motionblur")->setActivated();
-        getWidget<CheckBoxWidget>("dof")->setActivated();
-        getWidget<SpinnerWidget>("shadows")->setActivated();
-        getWidget<CheckBoxWidget>("mlaa")->setActivated();
-        getWidget<CheckBoxWidget>("ssao")->setActivated();
-        getWidget<CheckBoxWidget>("lightshaft")->setActivated();
-        getWidget<CheckBoxWidget>("ibl")->setActivated();
-        getWidget<CheckBoxWidget>("global_illumination")->setActivated();
-        getWidget<CheckBoxWidget>("glow")->setActivated();
-        getWidget<CheckBoxWidget>("bloom")->setActivated();
-    }
-    else
-    {
-        getWidget<CheckBoxWidget>("motionblur")->setDeactivated();
-        getWidget<CheckBoxWidget>("dof")->setDeactivated();
-        getWidget<SpinnerWidget>("shadows")->setDeactivated();
-        getWidget<CheckBoxWidget>("mlaa")->setDeactivated();
-        getWidget<CheckBoxWidget>("ssao")->setDeactivated();
-        getWidget<CheckBoxWidget>("lightshaft")->setDeactivated();
-        getWidget<CheckBoxWidget>("ibl")->setDeactivated();
-        getWidget<CheckBoxWidget>("global_illumination")->setDeactivated();
-        getWidget<CheckBoxWidget>("glow")->setDeactivated();
-        getWidget<CheckBoxWidget>("bloom")->setDeactivated();
-    }
+    bool light = getWidget<CheckBoxWidget>("dynamiclight")->getState();
+    getWidget<CheckBoxWidget>("motionblur")->setActive(light);
+    getWidget<CheckBoxWidget>("dof")->setActive(true);
+    getWidget<SpinnerWidget>("shadows")->setActive(light);
+    getWidget<CheckBoxWidget>("mlaa")->setActive(light);
+    getWidget<CheckBoxWidget>("ssao")->setActive(light);
+    getWidget<CheckBoxWidget>("lightshaft")->setActive(light);
+    getWidget<CheckBoxWidget>("ibl")->setActive(light);
+    getWidget<CheckBoxWidget>("global_illumination")->setActive(light);
+    getWidget<CheckBoxWidget>("glow")->setActive(light);
+    getWidget<CheckBoxWidget>("bloom")->setActive(light);
 
     if (!CVS->supportsShadows() && !CVS->supportsGlobalIllumination())
     {
-        getWidget<SpinnerWidget>("shadows")->setDeactivated();
-        getWidget<CheckBoxWidget>("global_illumination")->setDeactivated();
+        getWidget<SpinnerWidget>("shadows")->setActive(false);
+        getWidget<CheckBoxWidget>("global_illumination")->setActive(false);
     }
 }   // updateActivation
 

src/guiengine/widget.cpp
@@ -148,29 +148,15 @@ void Widget::elementRemoved()
 
 // -----------------------------------------------------------------------------
 
-void Widget::setActivated()
+void Widget::setActive(bool active)
 {
     // even if this one is already active, do it anyway on purpose, maybe the
     // children widgets need to be updated
-    m_deactivated = false;
+    m_deactivated = !active;
     const int count = m_children.size();
     for (int n=0; n<count; n++)
     {
-        m_children[n].setActivated();
-    }
-}
-
-// -----------------------------------------------------------------------------
-
-void Widget::setDeactivated()
-{
-    // even if this one is already inactive, do it anyway on purpose, maybe the
-    // children widgets need to be updated
-    m_deactivated = true;
-    const int count = m_children.size();
-    for (int n=0; n<count; n++)
-    {
-        m_children[n].setDeactivated();
+        m_children[n].setActive(active);
     }
 }
 

src/guiengine/widget.hpp
@@ -322,7 +322,7 @@ namespace GUIEngine
         /**
          * \brief Sets the widget (and its children, if any) visible or not.
          * Note that setting a widget invisible implicitely calls setDeactivated(), and setting
-         * it visible implicitely calls setActivated(). If you mix visiblity and (de)activated calls,
+         * it visible implicitely calls setActive(true). If you mix visiblity and (de)activated calls,
          * undefined behavior may ensue (like invisible but clickable buttons).
          */
         void setVisible(bool visible);
@@ -352,12 +352,11 @@ namespace GUIEngine
          * \{
          */
 
-        /** \brief undos setDeactivated() */
-        virtual void setActivated();
-
-        /** \brief greys out the widget, making it not clickable for the user */
-        virtual void setDeactivated();
-
+        /** \brief Sets an widget to be either activated or deactivated 
+         *  (i.e. greyed out)
+         *  \param active Active (true) or deactive (false). Defaults to 
+         *         true. */
+        virtual void setActive(bool active=true);
 
         /**
           * \}

src/guiengine/widgets/spinner_widget.cpp
@@ -395,36 +395,34 @@ void SpinnerWidget::setValue(irr::core::stringw new_value)
 
 // -----------------------------------------------------------------------------
 
-void SpinnerWidget::setActivated()
+void SpinnerWidget::setActive(bool active)
 {
-    Widget::setActivated();
+    Widget::setActive(active);
 
-    setText(L"");
-    if (m_customText.empty())
+    if (active)
     {
-        setValue( getValue() ); // Update the display
+        setText(L"");
+        if (m_customText.empty())
+        {
+            setValue(getValue()); // Update the display
+        }
+        else
+        {
+            setCustomText(m_customText);
+        }
     }
     else
     {
-        setCustomText(m_customText);
+        // Save it temporary because setValue(which is uses for update in
+        // this case) overwrites it
+        core::stringw customText = m_customText;
+        setText(L"-");
+        setValue(getValue()); // Update the display
+        m_customText = customText;
     }
-}
+}   // setActive
 
 // -----------------------------------------------------------------------------
-
-void SpinnerWidget::setDeactivated()
-{
-    Widget::setDeactivated();
-
-    // Save it temporary because setValue(which is uses for update in this case) overwrites it
-    core::stringw customText = m_customText; 
-    setText(L"-");
-    setValue( getValue() ); // Update the display
-    m_customText = customText;
-}
-
-// -----------------------------------------------------------------------------
-
 void SpinnerWidget::setCustomText(const core::stringw& text)
 {
     m_customText = text;

src/guiengine/widgets/spinner_widget.hpp
@@ -193,10 +193,7 @@ namespace GUIEngine
         
         // --------------------------------------------------------------------
         /** Override method from base class Widget */
-        virtual void setActivated();
-
-        /** Override method from base class Widget */
-        virtual void setDeactivated();
+        virtual void setActive(bool active = true);
 
         /** Display custom text in spinner */
         void setCustomText(const core::stringw& text);

src/guiengine/widgets/text_box_widget.cpp
@@ -174,30 +174,16 @@ void TextBoxWidget::elementRemoved()
 
 // -----------------------------------------------------------------------------
 
-void TextBoxWidget::setActivated()
+void TextBoxWidget::setActive(bool active)
 {
-    Widget::setActivated();
+    Widget::setActive(active);
 
     if (m_element != NULL)
     {
         IGUIEditBox* textCtrl = Widget::getIrrlichtElement<IGUIEditBox>();
         assert(textCtrl != NULL);
-        textCtrl->setEnabled(true);
+        textCtrl->setEnabled(active);
     }
-}
-
-// -----------------------------------------------------------------------------
-
-void TextBoxWidget::setDeactivated()
-{
-    Widget::setDeactivated();
-
-    if (m_element != NULL)
-    {
-        IGUIEditBox* textCtrl = Widget::getIrrlichtElement<IGUIEditBox>();
-        assert(textCtrl != NULL);
-        textCtrl->setEnabled(false);
-    }
-}
+}   // setActive
 
 // -----------------------------------------------------------------------------

src/guiengine/widgets/text_box_widget.hpp
@@ -73,10 +73,7 @@ namespace GUIEngine
         virtual void elementRemoved();
 
         /** Override method from base class Widget */
-        virtual void setActivated();
-
-        /** Override method from base class Widget */
-        virtual void setDeactivated();
+        virtual void setActive(bool active=true);
     };
 }
 

src/states_screens/addons_screen.cpp
@@ -143,7 +143,7 @@ void AddonsScreen::init()
     m_sort_default = true;
     m_sort_col = 0;
 
-    getWidget<GUIEngine::RibbonWidget>("category")->setDeactivated();
+    getWidget<GUIEngine::RibbonWidget>("category")->setActive(false);
 
     if(UserConfigParams::logAddons())
         Log::info("addons", "Using directory <%s>", file_manager->getAddonsDir().c_str());
@@ -157,10 +157,8 @@ void AddonsScreen::init()
     w_list->setIcons(m_icon_bank, (int)(m_icon_height));
 
     m_type = "kart";
-    if (UserConfigParams::m_internet_status != RequestManager::IPERM_ALLOWED)
-        getWidget<GUIEngine::IconButtonWidget>("reload")->setDeactivated();
-    else
-        getWidget<GUIEngine::IconButtonWidget>("reload")->setActivated();
+    bool ip = UserConfigParams::m_internet_status == RequestManager::IPERM_ALLOWED;
+    getWidget<GUIEngine::IconButtonWidget>("reload")->setActive(ip);
 
     // Reset filter.
     GUIEngine::TextBoxWidget* w_filter_name =
@@ -371,7 +369,7 @@ void AddonsScreen::loadList()
         }
     }
 
-    getWidget<GUIEngine::RibbonWidget>("category")->setActivated();
+    getWidget<GUIEngine::RibbonWidget>("category")->setActive(true);
     if(m_type == "kart")
         getWidget<GUIEngine::RibbonWidget>("category")->select("tab_kart",
                                                         PLAYER_ID_GAME_MASTER);

commit 3041792c4a04112848ad8b92bb92f6e737594966

Fixed minor inconsistency in video settings dialog.
Regression since c039b818f357e2d8f6295b2c96e47d147e6879f6

src/states_screens/dialogs/custom_video_settings.cpp
@@ -229,7 +229,7 @@ void CustomVideoSettingsDialog::updateActivation()
 {
     bool light = getWidget<CheckBoxWidget>("dynamiclight")->getState();
     getWidget<CheckBoxWidget>("motionblur")->setActive(light);
-    getWidget<CheckBoxWidget>("dof")->setActive(true);
+    getWidget<CheckBoxWidget>("dof")->setActive(light);
     getWidget<SpinnerWidget>("shadows")->setActive(light);
     getWidget<CheckBoxWidget>("mlaa")->setActive(light);
     getWidget<CheckBoxWidget>("ssao")->setActive(light);